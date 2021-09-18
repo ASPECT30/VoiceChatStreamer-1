62030129b6f239bb92f5596d26cc5b6630f54490
ASPECT30 <87013929+ASPECT30@users.noreply.github.com>
2021-09-18 23:11:13 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # PLEASE EDIT THIS FILE IF YOU DEPLOYING THIS BOT ON VPS. ELSE *DON'T* TOUCH THIS FILE.
 
 # Please edit 'CONFIG = False' to 'CONFIG = True'
-CONFIG = False
+CONFIG =True
 
 # Add your APP ID from my.telegram.org
-API_ID = 123456
+API_ID =6832604
 
 # Add your API HASH from my.telegram.org
-API_HASH = "xxxxxxxxxxxxxxxxxxx"
+API_HASH =9aeda7dfe6f9ac84c3432faa704193ed
 
 # Read readme and generate string session. Then add that to here.
-SESSION = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+SESSION =BQBwOuYRzoPcxEOujUoELeteeY-V8AmREFIeDgCRQlXRXSjyeMdGWZFi8FHIabLLOyew8x2V4lwp1jQe0rTM2wYLBbEQPZ_xIc3WRpg9tasNbs7dkGQnlBe6vMPaf1dtxqNk2MPjYDUQxfn_hbM_2AR5NuZCeBStLuzmk_mterlxog7kuBX9oBIZKDEU_CJPtDJKCKFtS7siRDo7kNAHt8GS6-hqU3LDE8Pq78ZwpssZVxj04WPrcshuYmR-qz4wMhkY437-QP7zVfGzpWmdJa9QeJe6B-0UuFiU5KQ4Ca8u7atDaBVLeHk09i44sH_9xEs3K-Dx7PkdMdcEA2KMoDHDcVPb0QA
